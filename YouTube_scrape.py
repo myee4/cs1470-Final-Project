fb29896b88dcc90ad2b702e975241b3e7c15a6ae
Matthew Yee <matthew_w_yee@brown.edu>
2023-05-02 14:30:10 -0400

    c

--- YouTube_scrape.py
+++ YouTube_scrape.py
@@ -12,28 +12,38 @@ video_id = 'lOg1yv8suZM'
 
 # Use the videos().list() method to retrieve information about the video
 # Video IDs to look up
-# video_ids = ['lOg1yv8suZM', '5TCGXyHzSSc', 'oave4Z939as']
+video_ids = ['lOg1yv8suZM', '5TCGXyHzSSc', 'oave4Z939as']
 
 # # Call the videos().list method to retrieve information about each video
 
+#USE FOR SEARCHING FOR VIDS
+# search_response = youtube.search().list(
+#     part='id',
+#     q='gaming',
+#     type='video',
+#     maxResults=500
+# ).execute()
 
+max_results = 50 #not sure why but this hates going over 50
+
+# Retrieve a list of videos sorted by date published
 search_response = youtube.search().list(
-    part='id',
-    q='gaming',
     type='video',
-    maxResults=500
+    order='date',
+    part='id',
+    maxResults=max_results
 ).execute()
 
 
-
+# THIS TOO FOR SEARCHING
 # Extract video IDs from the search response
 video_ids = [item['id']['videoId'] for item in search_response['items']]
 
 videos_response = youtube.videos().list(
     part='snippet,statistics',
-    id=','.join(video_ids)
+    id=','.join(video_ids) #change to video_ids if want more
 ).execute()
-
+count = 1
 # Iterate over the video responses to extract the desired information
 for video in videos_response['items']:
     # Extract video information
@@ -43,6 +53,14 @@ for video in videos_response['items']:
     view_count = video['statistics']['viewCount']
     like_count = video['statistics']['likeCount']
     date_published = datetime.strptime(video['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+    
+    channel_id = video['snippet']['channelId']
+    channel_response = youtube.channels().list(
+    id=channel_id,
+    part='statistics'
+    ).execute()
+    subscriber_count = channel_response['items'][0]['statistics']['subscriberCount']
+
 
     # Print the extracted information
     print(f'Video ID: {video_id}')
@@ -51,4 +69,6 @@ for video in videos_response['items']:
     print(f'View count: {view_count}')
     print(f'Like count: {like_count}')
     print(f'Date published: {date_published}')
-    print('--------------------------')
+    print(f'Subscribers: {subscriber_count}')
+    print('--------------------------  ' + str(count))
+    count +=1 
