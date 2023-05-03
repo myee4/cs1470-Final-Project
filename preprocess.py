b1297226878ce1aecc5ea25bcb9c8d0c335c3648
Matthew Yee <matthew_w_yee@brown.edu>
2023-05-02 20:57:06 -0400

    preprocessing done?

--- preprocess.py
+++ preprocess.py
@@ -13,38 +13,42 @@ import datetime
 import time
 
 def preprocess_words(titles, tags, max_window_size=50):
-    words_lists = tf.concat([tags, titles], axis = 1)
-    for i, words in enumerate(words_lists):
+    # words_lists = np.concatenate([[tags], np.asarray(np.expand_dims(titles, axis = 1)).T], axis = 1)
+    texts_lists = [None] * titles.shape[0]
+    for i,  (tag, title) in enumerate(zip(tags, titles)):
         # Taken from:
         # https://towardsdatascience.com/image-captions-with-attention-in-tensorflow-step-by-step-927dad3569fa
-
+        single_text = tag + " " + title
         # Convert the caption to lowercase, and then remove all special characters from it
-        words_nopunct = re.sub(r"[^a-zA-Z0-9]+", ' ', words.lower())
-      
+        text_nopunct = re.sub(r"[^a-zA-Z0-9]+", ' ', single_text.lower())
         # Split the caption into separate words, and collect all words which are more than 
         # one character and which contain only alphabets (ie. discard words with mixed alpha-numerics)
-        clean_words = [word for word in words_nopunct.split() if ((len(word) > 1) and (word.isalpha()))]
+        clean_text = [word for word in text_nopunct.split() if ((len(word) > 1) and (word.isalpha()))]
       
         # Join those words into a string
-        words_new = ['<start>'] + clean_words[:max_window_size-1] + ['<end>']
+        words_new = ['<start>'] + clean_text[:max_window_size-1] + ['<end>']
       
         # Replace the old caption in the captions list with this new cleaned caption
-        words_lists[i] = words_new
+        texts_lists[i] = words_new
+    return texts_lists
     
 
 def get_images_from_url(images):
+    image_lists = [None] * images.shape[0]
     for i, url in enumerate(images):
         img_data = requests.get(url).content
         img = Image.open(BytesIO(img_data))
-        images[i] = np.asarray(img.getdata())
-
+        image_lists[i] = np.asarray(img.getdata())
+    return image_lists
 
 def get_num_from_date(dates):
+    date_lists = [None] * dates.shape[0]
     for i, date in enumerate(dates):
-        utc_date = datetime.datetime.fromisoformat(date).replace(tzinfo=datetime.timezone.utc)
+        iso_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
+        utc_date = datetime.datetime.fromisoformat(str(iso_date)).replace(tzinfo=datetime.timezone.utc)
         unix_timestamp = int(utc_date.timestamp())
-        dates[i] = unix_timestamp
-
+        date_lists[i] = unix_timestamp
+    return date_lists
 
 def load_data(data_file):
     view_data = pd.read_csv(data_file)
@@ -52,15 +56,17 @@ def load_data(data_file):
     ids = X['video_id']
     num_in = int(ids.shape[0])
     indices = tf.random.shuffle(range(num_in))
-    ids = tf.gather(ids, indices)
+    #ids = np.take(ids, indices)
     thumbnails = get_images_from_url(X['thumbnail_link'])
-    thumbnails = tf.gather(thumbnails, indices)
+    #thumbnails = np.take(thumbnails, indices)
     dates = get_num_from_date(X['publishedAt'])
-    dates = tf.gather(dates, indices)
-    likes = tf.gather(X['likes'], indices)
-    views = tf.gather(X['view_count'], indices)
+    # dates = np.take(dates, indices)
+    likes = X['likes']
+    # likes = np.take(likes], indices)
+    views = X['view_count']
+    # views = np.take(views , indices)
     text = preprocess_words(X['title'], X['tags'], 50) # window of 50
-    text = tf.gather(text, indices)
+    # text = np.take(text, indices)
     train_num = int( 0.7 * num_in)
     train_text = text[:train_num]
     test_text = text[train_num:]
@@ -72,7 +78,7 @@ def load_data(data_file):
         for text in texts:
             for index, word in enumerate(text):
                 if word_count[word] <= minimum_frequency:
-                    caption[index] = '<unk>'
+                    text[index] = '<unk>'
 
     unk_text(train_text, 50)
     unk_text(test_text, 50)
@@ -125,6 +131,6 @@ def create_pickle(data_folder, data_file):
 if __name__ == '__main__':
     ## Download this and put the Images and captions.txt indo your ../data directory
     ## Flickr 8k Dataset: https://www.kaggle.com/datasets/adityajn105/flickr8k?resource=download
-    data_file = r'C:\Users\matth\OneDrive\Desktop\DEEP_Learning\cs1470-Final-Project\kaggle_data\US_youtube_trending_data.csv'
+    data_file = r'C:\Users\matth\OneDrive\Desktop\DEEP_Learning\cs1470-Final-Project\kaggle_data\sample_trending_data.csv'
     data_folder = r'C:\Users\matth\OneDrive\Desktop\DEEP_Learning\cs1470-Final-Project\kaggle_data'
     create_pickle(data_folder, data_file)
